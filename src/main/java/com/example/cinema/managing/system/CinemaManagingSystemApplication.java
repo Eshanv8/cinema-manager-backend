0e0082b64445cf2b7b78b53abdb9d757348f7219
Eshanv8 <eshanv8@gmail.com>
2025-11-06 17:10:54 +0530

    first commit after initiating

---
+++ src/main/java/com/example/cinema/managing/system/CinemaManagingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.cinema.managing.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaManagingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaManagingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cinema/managing/system/CinemaManagingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cinema.managing.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaManagingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
